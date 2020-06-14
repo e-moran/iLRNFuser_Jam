adb73baf2d68d34f100a2fb2746bada843790e78
Eoin <eoin.t.moran00@gmail.com>
2020-06-14 23:59:17 +0100

    Added code to change the send planet button sprite to change it when the mouse hovers over it and clicks it

--- Events.cs
+++ Events.cs
@@ -10,4 +10,6 @@ public class Events: MonoBehaviour
     {
         OnNewRound?.Invoke();
     }
+    
+    
 }
--- SendPlanetButton.cs
+++ SendPlanetButton.cs
@@ -1,17 +1,77 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class SendPlanetButton : MonoBehaviour
 {
     private Events _events;
+    private Sprite[] _stateSprites;
+    private Action RedrawButton;
+    private SpriteRenderer _spriteRenderer;
+
+    private int state;
+    private bool hovering;
+    
+    private const int DEFAULT = 0;
+    private const int HOVER = 1;
+    private const int CLICKED = 2;
+
     void Start()
     {
+        RedrawButton += DrawButton;
         _events = GameObject.Find("GameManager").GetComponent<Events>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        // Initialise all the states for the button to be in
+        _stateSprites = new Sprite[3];
+        _stateSprites[0] = Resources.Load<Sprite>("Images/SendPlanetButton/Default");
+        _stateSprites[1] = Resources.Load<Sprite>("Images/SendPlanetButton/Hover");
+        _stateSprites[2] = Resources.Load<Sprite>("Images/SendPlanetButton/Clicked");
+
+        state = DEFAULT;
+        RedrawButton.Invoke();
+    }
+
+    void DrawButton()
+    {
+        _spriteRenderer.sprite = _stateSprites[state];
     }
 
     void OnMouseDown()
     {
-        // TODO implement planet validation
         _events.InvokeNewRound();
+        StartCoroutine(DoAndResetButtonClick());
+    }
+
+    IEnumerator DoAndResetButtonClick()
+    {
+        state = CLICKED;
+        RedrawButton?.Invoke();
+        yield return new WaitForSeconds(0.2f);
+        state = hovering ? HOVER : DEFAULT;
+        RedrawButton?.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        if (state == DEFAULT)
+        {
+            state = HOVER;
+            RedrawButton?.Invoke();
+        }
+
+        hovering = true;
+    }
+
+    private void OnMouseExit()
+    {
+        hovering = false;
+        if (state == HOVER)
+        {
+            state = DEFAULT;
+            RedrawButton?.Invoke();
+        }
+
+        hovering = false;
     }
 }
