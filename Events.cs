29b18b9967422a4e58f0da363c1223cc4636be0a
Eoin <eoin.t.moran00@gmail.com>
2020-06-14 23:42:24 +0100

    Added a new class for a sprite-based button instead of the Unity UI button

--- Events.cs
+++ Events.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Events: MonoBehaviour
 {
 
     public event Action OnNewRound;
-
-    private void Start()
-    {
-        GameObject.Find("NewRoundButton").GetComponent<Button>().onClick.AddListener(InvokeNewRound);
-    }
 
     public void InvokeNewRound()
     {
---
+++ SendPlanetButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class SendPlanetButton : MonoBehaviour
+{
+    private Events _events;
+    void Start()
+    {
+        _events = GameObject.Find("GameManager").GetComponent<Events>();
+    }
+
+    void OnMouseDown()
+    {
+        // TODO implement planet validation
+        _events.InvokeNewRound();
+    }
+}
