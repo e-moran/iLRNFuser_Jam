8368360fa7c8f13128230dc8cadde9aacf678c9d
Eoin <eoin.t.moran00@gmail.com>
2020-06-14 23:28:18 +0100

    Added randomisation of planets on new round

---
+++ Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Events: MonoBehaviour
+{
+
+    public event Action OnNewRound;
+
+    private void Start()
+    {
+        GameObject.Find("NewRoundButton").GetComponent<Button>().onClick.AddListener(InvokeNewRound);
+    }
+
+    public void InvokeNewRound()
+    {
+        OnNewRound?.Invoke();
+    }
+}
---
+++ Planet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Planet : MonoBehaviour
+{
+    private Sprite[] _planetSprites;
+    private Events _events;
+    private SpriteRenderer _spriteRenderer;
+    
+    void Start()
+    {
+        _planetSprites = Resources.LoadAll<Sprite>("Images/Planets");
+        _events = GameObject.Find("GameManager").GetComponent<Events>();
+        _events.OnNewRound += OnNewRound;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _events.InvokeNewRound();
+    }
+
+    void OnNewRound()
+    {
+        _spriteRenderer.sprite = _planetSprites[Random.Range(0, _planetSprites.Length)];
+        transform.eulerAngles = new Vector3(0, 0, Random.Range(-30.0f, 30.0f));
+    }
+
+    
+}
