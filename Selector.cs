e1cf499baf9a402b493c515cecf44b37eff932e5
Eoin <eoin.t.moran00@gmail.com>
2020-06-16 00:04:55 +0100

    Added code for radio-button style UI elements for the selections panel.

---
+++ Selector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Selector : MonoBehaviour
+{
+    public int selectorId = 0;
+    
+    private SelectorGroup _group;
+    private SpriteRenderer _renderer;
+    
+    void Start()
+    {
+        _group = transform.parent.GetComponent<SelectorGroup>(); // Get the parent which must contain a selector group
+        _group.OnNewSelection += OnNewSelection;
+        _renderer = GetComponent<SpriteRenderer>();
+        _renderer.enabled = false;
+    }
+
+    void OnNewSelection(int id)
+    {
+        _renderer.enabled = id == selectorId; // Selector shadow will be visible if this is the correct selector
+    }
+
+    void OnMouseDown()
+    {
+        _group.InvokeNewSelection(selectorId);
+    }
+}
---
+++ SelectorGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class SelectorGroup : MonoBehaviour
+{
+    public Action<int> OnNewSelection;
+    public int Selected { get; private set; }
+
+    public void InvokeNewSelection(int selector)
+    {
+        Selected = selector;
+        OnNewSelection?.Invoke(selector);
+    }
+}
