835809ea8ff0e7306e32ce887c39a8d890b1a9ba
Eoin <eoin.t.moran00@gmail.com>
2020-06-16 00:43:53 +0100

    Added score to the UI

---
+++ ScoreText.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    public GameController gameController;
+    private TextMeshPro _text;
+    
+    void Start()
+    {
+        gameController.ScoreChanged += RefreshScore;
+        _text = GetComponent<TextMeshPro>();
+    }
+
+    void RefreshScore(int newScore)
+    {
+        _text.text = newScore.ToString();
+    }
+}
