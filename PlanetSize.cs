754d0ec1c79cb24f82f4077905a23a2d8a584b46
Eoin <eoin.t.moran00@gmail.com>
2020-06-17 20:21:29 +0100

    Setup Coroutine for lerping between two scales instead of instantly transitioning

--- PlanetSize.cs
+++ PlanetSize.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlanetSize : MonoBehaviour
 {
+    public float lerpTimer = 0.2f;
+
     private readonly float[] _scaleMapping = {
-        0.35f, 0.5f, 0.65f
+        0.4f, 0.5f, 0.6f // Our scales for the different options of planet sizes
     };
 
     public SelectorGroup selectorGroup;
@@ -15,6 +18,23 @@ public class PlanetSize : MonoBehaviour
 
     void OnChanged()
     {
-        transform.localScale = (new Vector3(1, 1, 0)) * _scaleMapping[selectorGroup.Selected];
+        StartCoroutine(LerpedScale());
+    }
+
+    IEnumerator LerpedScale()
+    {
+        float elapsedTime = 0;
+        Vector3 startScale = transform.localScale;
+
+        // Keep lerp'ing every frame until we reach the desired scale
+        while (elapsedTime < lerpTimer)
+        {
+            yield return new WaitForEndOfFrame();
+            elapsedTime += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(
+                startScale, 
+                new Vector3(1, 1, 0) * _scaleMapping[selectorGroup.Selected], 
+                (elapsedTime / lerpTimer));
+        }
     }
 }
