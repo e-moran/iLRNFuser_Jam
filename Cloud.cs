115173e39636b759a6fe70d78ad4a94cbd8e5fdc
Eoin <eoin.t.moran00@gmail.com>
2020-06-21 01:09:42 +0100

    Clouds with working opacity

---
+++ Cloud.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float movementPeriod = 1f;
+    public float yTravel = 0.05f; // The range up and down the cloud moves as it bobs
+    
+    private float _topY;
+    private float _bottomY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider2D collider = GetComponent<PolygonCollider2D>();
+        do
+        {
+            
+        } while (Physics2D.IsTouchingLayers(collider));
+        
+        _topY = transform.position.y + yTravel;
+        _bottomY = transform.position.y - yTravel;
+    }
+    
+    void Update()
+    {
+        MovementDirection direction = (int) Math.Floor(Time.time / movementPeriod) % 2 == 0 // Flip between the two y-directions every time we pass the period
+            ? MovementDirection.Down
+            : MovementDirection.Up;
+
+        Vector3 startPos = new Vector3(transform.position.x, direction == MovementDirection.Up ? _bottomY : _topY, 0);
+        Vector3 endPos = new Vector3(transform.position.x, direction == MovementDirection.Up ? _topY : _bottomY, 0);
+
+        float lerpFactor = Time.time % movementPeriod;
+
+        transform.position = Vector3.Lerp(startPos, endPos, lerpFactor);
+    }
+
+    enum MovementDirection
+    {
+        Up,
+        Down
+    }
+}
---
+++ CloudManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CloudManager : MonoBehaviour
+{
+    public SelectorGroup selectorGroup;
+    public SpriteRenderer[] clouds;
+
+    private float[] _opacityMapping =
+    {
+        0.0f,
+        0.4f,
+        0.8f
+    };
+
+    void Start()
+    {
+        selectorGroup.NewSelection += OnChanged;
+    }
+
+    void OnChanged()
+    {
+        foreach (var c in clouds)
+        {
+            c.color = new Color(1, 1, 1, _opacityMapping[selectorGroup.Selected]);
+        }
+    }
+}
