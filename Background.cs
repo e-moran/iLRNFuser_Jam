d656e728b3a8087e9e48a22c0a49a1f94ca9f353
Eoin <eoin.t.moran00@gmail.com>
2020-06-21 15:50:13 +0100

    Background changing logic

---
+++ Background.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public Sprite[] backgrounds;
+    public SelectorGroup selector;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        selector.NewSelection += OnChanged;
+    }
+
+    private void OnChanged()
+    {
+        _spriteRenderer.sprite = backgrounds[selector.Selected];
+    }
+}
--- SelectorGroup.cs
+++ SelectorGroup.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 public class SelectorGroup : MonoBehaviour
 {
     public Action NewSelection;
+    public int startingValue = 1;
     public int Selected { get; private set; } = 1;
 
+    void Awake()
+    {
+        Selected = startingValue;
+    }
+    
     public void InvokeNewSelection(int selector)
     {
         Selected = selector;
